2283e7287e2ed56a480f09ef35a6be6da77a6e43
worldofelisa <worldofelisa@gmail.com>
2022-01-20 10:51:47 +0000

    Added to the code so the first letter is capitalized, to follow standard grammatical rules.

---
+++ Oob/oob.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "Hello, what's your name?\n";
+
+$name = readline(">> ");
+$name = strtolower($name);
+$letters = str_split($name);
+$new_name = "";
+
+foreach ($letters as $letter) {
+    switch ($letter) {
+        case "a":
+        case "e":
+        case "i":
+        case "o":
+        case "u":
+            $new_name .= "oob";
+            break;
+        default:
+            $new_name .= "$letter";
+    }
+}
+$new_name= ucfirst($new_name);
+echo "Hello $new_name!\n";
